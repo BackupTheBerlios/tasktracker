d785ade13905a59de41e145feae3a9dc39903e31
emilkangas <emilkangas>
2006-10-26 15:01:59 +0000

    test implemetation for a veeery simple user

--- tasktracker/src/com/comp/tasktracker/model/User.java
+++ tasktracker/src/com/comp/tasktracker/model/User.java
@@ -4,11 +4,49 @@ public class User{
 
     private long id;
 
-    private Settings settings;
-
     private String surName;
 
     private String familyName;
 
-    public Settings mySettings;
+    private Settings settings;
+
+    public User(){}
+
+    public User(String surName, String familyName){
+        this.surName = surName;
+        this.familyName = familyName;
+    }
+
+    public String getFamilyName(){
+        return familyName;
+    }
+
+    public void setFamilyName(String familyName){
+        this.familyName = familyName;
+    }
+
+    public Settings getSettings(){
+        return settings;
+    }
+
+    public void setSettings(Settings settings){
+        this.settings = settings;
+    }
+
+    public String getSurName(){
+        return surName;
+    }
+
+    public void setSurName(String surName){
+        this.surName = surName;
+    }
+
+    public long getId(){
+        return id;
+    }
+
+    public void setId(long id){
+        this.id = id;
+    }
+
 }
