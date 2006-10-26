dcb78fb593560ba020ba6f6915cf73daf99c4609
emilkangas <emilkangas>
2006-10-26 15:07:07 +0000

    Variuos constants used primaruily by the web part can reside here For now - mappings for page navigation

---
+++ tasktracker/src/com/comp/tasktracker/web/constants/WebConst.java
@@ -0,0 +1,11 @@
+package com.comp.tasktracker.web.constants;
+
+public class WebConst{
+
+    /*
+     * Mapping Constants
+     */
+    public static final String SHOW_USERS = "/show_users.jsp";
+    public static final String HOME = "/";
+
+}
