ce68413f6686eb4cb94bd53625a80ad8eeb70efc
emilkangas <emilkangas>
2006-10-26 15:03:52 +0000

    Updated Hibernate utility code

--- tasktracker/src/com/comp/tasktracker/persistence/HibernateAppListener.java
+++ tasktracker/src/com/comp/tasktracker/persistence/HibernateAppListener.java
@@ -1,3 +1,34 @@
 package com.comp.tasktracker.persistence;
 
-public class HibernateAppListener {}
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextListener;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletException;
+
+import org.apache.log4j.Logger;
+import org.hibernate.Session;
+
+public class HibernateAppListener implements ServletContextListener{
+
+    private static final boolean DEBUG = true;
+
+    private static final String hibernateUtilClassName = "com.comp.tasktracker.persistence.HibernateUtil";
+
+    /* Application Startup Event */
+    public void contextInitialized(ServletContextEvent ce){
+
+        try{
+            if(DEBUG)System.out.println(this.getClass().getName() + ".contextInitialized(ServletContextEvent ce) ->");
+
+            Class.forName(hibernateUtilClassName).newInstance();
+            if(DEBUG)System.out.println("In HibernateAppListener, Class.forName for " + hibernateUtilClassName + " successful");
+        }
+        catch(Exception e){
+            if(DEBUG)System.out.println("In HibernateAppListener, Class.forName for " + hibernateUtilClassName + " throws Exception");
+        }
+    }
+
+    /* Application Shutdown Event */
+    public void contextDestroyed(ServletContextEvent ce){
+    }
+}
--- tasktracker/src/com/comp/tasktracker/persistence/HibernateUtil.java
+++ tasktracker/src/com/comp/tasktracker/persistence/HibernateUtil.java
@@ -1,3 +1,35 @@
 package com.comp.tasktracker.persistence;
 
-public class HibernateUtil {}
+import java.util.logging.Logger;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil{
+
+    private static final boolean DEBUG = true;
+
+    private static final SessionFactory sessionFactory;
+
+    static{
+        try{
+            if(DEBUG)
+                System.out.println("In HibernateUtil try-clause");
+            if(DEBUG)
+                System.out.println("In HibernateUtil try-clause via loggerConnect DEBUG*****");
+
+            // Create the SessionFactory from hibernate.cfg.xml
+            sessionFactory = new Configuration().configure()
+                    .buildSessionFactory();
+        }
+        catch(Throwable ex){
+            // Make sure you log the exception, as it might be swallowed
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory(){
+        return sessionFactory;
+    }
+}
