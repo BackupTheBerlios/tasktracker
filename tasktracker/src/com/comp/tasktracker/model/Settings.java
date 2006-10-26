d85f5935e2e39f96794f4a5ae243a6076278b8da
emilkangas <emilkangas>
2006-10-26 15:01:40 +0000

    test implemetation for a veeery simple setting

--- tasktracker/src/com/comp/tasktracker/model/Settings.java
+++ tasktracker/src/com/comp/tasktracker/model/Settings.java
@@ -5,4 +5,28 @@ public class Settings{
     private long id;
 
     private int workDayMinutes;
+
+    public Settings(){}
+
+    public Settings(int workDayMinutes){
+        this.workDayMinutes = workDayMinutes;
+    }
+
+    public int getWorkDayMinutes(){
+        return workDayMinutes;
+    }
+
+    public void setWorkDayMinutes(int workDayMinutes){
+        this.workDayMinutes = workDayMinutes;
+    }
+
+    public long getId(){
+        return id;
+    }
+
+    public void setId(long id){
+        this.id = id;
+    }
+
+    
 }
