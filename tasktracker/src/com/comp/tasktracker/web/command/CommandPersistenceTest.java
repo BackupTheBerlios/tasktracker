ca87e63a359e17dbea3b1504dcb5c6dba0949fd1
emilkangas <emilkangas>
2006-10-26 15:09:36 +0000

    not really used..

--- tasktracker/src/com/comp/tasktracker/web/command/CommandPersistenceTest.java
+++ tasktracker/src/com/comp/tasktracker/web/command/CommandPersistenceTest.java
@@ -3,13 +3,15 @@ package com.comp.tasktracker.web.command;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.comp.tasktracker.web.constants.WebConst;
+
 public class CommandPersistenceTest extends WebCommand {
 
     @Override
     public String execute(HttpServletRequest request,
             HttpServletResponse response) {
         // TODO Auto-generated method stub
-        return "/";
+        return WebConst.HOME;
     }
 
 }
